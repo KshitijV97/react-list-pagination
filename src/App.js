56508cfc5d62442d1ff518197daf6fe9d435953a
KshitijV97 <writetokshitij@gmail.com>
2021-08-31 21:26:58 +0530

    Page selection works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Posts } from './components/Posts';
+import { Pagination } from './components/Pagination';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 function App() {
 	const [posts, setPosts] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [postsPerPage, setPostsPerPage] = useState(10);
+	const [postsPerPage] = useState(10);
 
 	useEffect(() => {
 		const fetchPosts = async () => {
@@ -20,11 +24,25 @@ function App() {
 		fetchPosts();
 	}, []);
 
-	console.log(posts);
+	// Get current Posts
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+	const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+	// Change page
+	const paginate = (pageNumber) => {
+		setCurrentPage(pageNumber);
+	};
+
 	return (
 		<div className='container mt-5'>
 			<h1 className='text-primary mb-3'>My Blog</h1>
-			<Posts posts={posts} loading={loading} />
+			<Posts posts={currentPosts} loading={loading} />
+			<Pagination
+				postsPerPage={postsPerPage}
+				totalPosts={posts.length}
+				paginate={paginate}
+			/>
 		</div>
 	);
 }
