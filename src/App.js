a314754bdca943661ed68eead1cb98977bc788a1
KshitijV97 <writetokshitij@gmail.com>
2021-08-31 21:03:46 +0530

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Posts } from './components/Posts';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [postsPerPage, setPostsPerPage] = useState(10);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			setLoading(true);
+			const res = await axios.get(
+				'https://jsonplaceholder.typicode.com/posts'
+			);
+			setPosts(res.data);
+			setLoading(false);
+		};
+		fetchPosts();
+	}, []);
+
+	console.log(posts);
+	return (
+		<div className='container mt-5'>
+			<h1 className='text-primary mb-3'>My Blog</h1>
+			<Posts posts={posts} loading={loading} />
+		</div>
+	);
 }
 
 export default App;
